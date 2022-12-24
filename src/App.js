2a94b8b1fbb5bc9cabb4a5b32b7a3db587f99258
ibrahimnd2000 <ibrahimnd2000@gmail.com>
2022-12-24 21:13:05 +0500

    Converted function component to class component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
-  return <div></div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 export default App;
