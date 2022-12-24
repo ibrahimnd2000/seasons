24f0786e6187fcb9aeb4b2a12645fee99d2bd43f
ibrahimnd2000 <ibrahimnd2000@gmail.com>
2022-12-24 21:08:57 +0500

    Added navigator api code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
   return <div></div>;
 };
 
